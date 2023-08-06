efe6dbb9181e8891fa3aa186916fa8242ecd0981
lash <dev@holbrook.no>
2023-08-06 16:19:54 +0100

    Upgrade deps

--- python/run_tests.sh
+++ python/run_tests.sh
@@ -4,6 +4,7 @@ set -e
 set -x
 default_pythonpath=$PYTHONPATH:.
 export PYTHONPATH=${default_pythonpath:-.}
+>&2 echo "using pythonpath $PYTHONPATH"
 for f in `ls tests/*.py`; do
 	python $f
 	if [ $? -gt 0 ]; then
